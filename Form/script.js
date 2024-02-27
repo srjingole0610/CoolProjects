47e95f86bd3b0dc1e66f2233182b23c88efd80f7
Suraj Ingole <suraj.ingole0610@gmail.com>
2024-02-27 10:23:40 +0530

    Changes related to form control

---
+++ Form/script.js
@@ -0,0 +1,7 @@
+const labels = document.querySelectorAll('.form-control label');
+labels.forEach(label =>{
+    label.innerHTML = label.innerText
+                .split('')
+                .map((letter, i) => `<span style="transition-delay:${i*60}ms">${letter}</span>`)
+                .join('');
+})
